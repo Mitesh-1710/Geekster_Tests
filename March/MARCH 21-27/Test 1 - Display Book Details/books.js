38d19e25cbaaf552a9366aafa965facd2a563418
Mitesh Mayavanshi <82865550+Mitesh-1710@users.noreply.github.com>
2022-03-27 12:53:05 +0530

    Add files via upload

---
+++ March/MARCH 21-27/Test 1 - Display Book Details/books.js
@@ -0,0 +1,29 @@
+"strict mode"
+
+var library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }
+ ];
+
+ for(let i = 0; i<library.length; i++)
+ {
+    console.log("\n");
+    console.log("---------------- Book : " + i + " -----------------------");
+    console.log("Author: " + library[i].author );
+    console.log("Title: " + library[i].title);
+    console.log("Reading  Status: " + library[i].readingStatus);
+    console.log("\n");
+ }
